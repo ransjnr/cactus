f97d6daeb67c8d2f6beeb03e92a79bcf99cdddee
justinl66 <justinfinity7@gmail.com>
2026-02-20 01:39:10 -0800

    Add native tests
    Signed-off-by: justinl66 <justinfinity7@gmail.com>

---
+++ native-tests/flutter/android/app/src/main/kotlin/com/cactus/cactus_flutter_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.cactus.cactus_flutter_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ native-tests/ios/CactusSwiftTest/CactusSwiftTest/CactusSwiftTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CactusSwiftTestApp.swift
+//  CactusSwiftTest
+//
+//  Created by Justin L on 2/20/26.
+//
+
+import SwiftUI
+
+@main
+struct CactusSwiftTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
